2aa19d7d82274550adeec313adbe2eb34ae9728c
Travis <travis.williams94@gmail.com>
2023-07-03 20:27:43 -0600

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        matched_word_list = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                matched_word_list.append(word)
+
+        return matched_word_list
